778a7e07e5c63ba7e521086c8bbde003bb286265
chiboycalix <igwechinonso77@gmail.com>
2019-02-03 22:56:45 +0100

    - ft(user sign in) setup user sign in functionality - create user sign in route - create user sign controller function - create user sign in validation middleware [Maintain #163684046]

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import UserController from '../controllers/UserController';
-import signupValidation from '../middlewares/validation';
+import validation from '../middlewares/validation';
 
 
 const router = express.Router();
 
-
+const { signupValidation, loginValidation } = validation;
 router.post('/auth/signup', signupValidation, new UserController().createUser);
+router.post('/auth/login', loginValidation, new UserController().loginUser);
 
 export default router;
