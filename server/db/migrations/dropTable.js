ba67054ce0d1ccb3793ccea2505a73aaad3e4b4d
chiboycalix <igwechinonso77@gmail.com>
2019-02-03 21:41:11 +0100

    ft(user-signup) setup user registration functionalities create user signup route create user signup controller create middleware for signup validation encrypt user password using bcrypt before saving to database [Maintains ##163683285]

--- server/db/migrations/dropTable.js
+++ server/db/migrations/dropTable.js
@@ -3,27 +3,6 @@ import db from '../index';
 
 const tableNames = ['users', 'parties', 'offices', 'candidate', 'vote'];
 
-// const query1 = 'DROP TABLE IF EXISTS users CASCADE';
-// const query2 = 'DROP TABLE IF EXISTS parties CASCADE';
-// const query3 = 'DROP TABLE IF EXISTS offices CASCADE';
-// const query4 = 'DROP TABLE IF EXISTS candidate CASCADE';
-// const query5 = 'DROP TABLE IF EXISTS vote CASCADE';
-
-// db.query(query1, (error) => {
-//   if (error) throw error;
-//   db.query(query2, (error) => {
-//     if (error) throw error;
-//     db.query(query3, (error) => {
-//       if (error) throw error;
-//       db.query(query4, (error) => {
-//         if (error) throw error;
-//         db.query(query5, (error) => {
-//           if (error) throw error;
-//         });
-//       });
-//     });
-//   });
-// });
 
 db.connect((error, client) => {
   if (error) throw error;
